e24c6c511227a2ad311df203a6cc43b33508cdfc
jeffdatura <76278214+jeffdatura@users.noreply.github.com>
2023-12-22 23:38:22 +0100

    Update

---
+++ contrllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+ 
+class HomeController
+  {
+    
+  public function index()
+  {
+    require_once 'views/form.php';
+  }
+
+  public function info()
+  {
+    require_once 'views/info.php';
+  }
+    
+}
