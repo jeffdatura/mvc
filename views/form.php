eeed84c44391a17761c512d27114f720fba29857
jeffdatura <76278214+jeffdatura@users.noreply.github.com>
2023-12-22 23:44:18 +0100

    Update

---
+++ views/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Home</title>
+    </head>
+    <body>
+      <h1>Bonjour</h1>
+      <p>
+        <a href='/mvc/info'>info</a>
+      </p>
+    </body>
+</html>
